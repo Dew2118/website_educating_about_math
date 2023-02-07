ca837562960cc0898281a57f5589c8e3395a162f
Dew2118 <kavin2118@gmail.com>
2023-02-07 18:10:14 +0700

    testing alert js on mouseover and mouseout event listener

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 const images = document.getElementsByTagName('img');
-
 for (const image of images) {
   image.addEventListener('mouseover', () => {
+    alert("MOUSEOVER!")
     image.style.width = '150%';
   });
  
   image.addEventListener('mouseout', () => {
+    alert("MOUSEOUT!")
     image.style.width = '100%';
   });
 }
